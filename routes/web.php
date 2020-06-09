9b005888ac68fd5808cb870a074444198296cb78
junghyunkim573 <assakjh79@gmail.com>
2020-06-09 21:06:52 +0900

    authentiacation

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,32 @@
 
 Route::get('/','welcomecontroller@index');
 
-Route::resource('articles','articlescontroller');
+Route::get('auth/login',function(){
+	$credentials=[
+		'email'=>'assakjh79@gmail.com',
+		'password'=>'k124j248h!'
+	];
+
+	if(! auth()->attempt($credentials)){
+		return '로그인 정보가 정확하지 않습니다.';
+	}
+
+	return redirect('protected');
+});
+
+Route::get('protected',['middleware'=>'auth',function(){
+	
+}]);
+
+Route::get('auth/logout',function(){
+	auth()->logout();
+
+	return '또 뵈요~';
+
+});
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
